e0407b7ad97ec7469752ddd9bdbf611514692f33
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-31 16:42:28 +0700

    Actuator

---
+++ Actuator.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("no water detected")
+       
+        GPIO.output(18,GPIO.HIGH)
+        
+    else:
+        print("water detected")
+        GPIO.output(18,GPIO.LOW)
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) # let us know when pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback) #assign function to GPIO PIN, Run function on change
+
+#infinite loop
+while True:
+    time.sleep(1)

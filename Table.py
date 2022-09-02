0a50e1b77b5e51be59ca01e334597f4ff55c3ad6
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-02 15:10:51 +0700

    Testing

--- Table.py
+++ Table.py
@@ -1,41 +1,25 @@
-# importing required libraries
-
-import mysql.connector
-
-  
+# Store sensor data and datetime in a persistent media (MySQL or local file) (6)
 
 dataBase = mysql.connector.connect(
 
-  host ="localhost",
+    host="localhost",
 
-  user ="root",
+    user="root",
 
-  passwd ="",
+    passwd="",
 
-  database = "hdsd"
+    database="hdsd"
 )
- 
-# preparing a cursor object
+
 
 cursorObject = dataBase.cursor()
-
-  
-
-sql = "INSERT INTO STUDENT (NAME, EMAIL) VALUES (%s, %s) "
-
-val = ("Yuki Sato", "lhsato.kh@gmail.com")
-
-   
-cursorObject.execute(sql, val)
-dataBase.commit()
-
-val = ("Karla Sato", "kmsato15@gmail.com")
-cursorObject.execute(sql, val)
-dataBase.commit()
-   
-val = ("Soy Hok", "soyhok2018@gmail.com")
-cursorObject.execute(sql, val)
-dataBase.commit()
-
-# disconnecting from server
-dataBase.close()
+f = open("Report.txt", "r")
+end_of_file = f.readline()
+for x in f:
+    res = x.split()
+    sql = "INSERT INTO tbl_STUDENT (NAME, EMAIL) VALUES (%s, %s)"
+    val = (str(res[0]), str(res[1]))
+    cursorObject.execute(sql, val)
+    dataBase.commit()
+    if not end_of_file:
+        break

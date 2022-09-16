fa210d66954b71a04b661b662b31ba1f5e42e4a5
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-16 13:16:38 +0700

    Testing

---
+++ testPump.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime
+from gpiozero import LED
+import socket
+
+led = LED(18)
+
+def command():
+    for i in range(0, 30):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        time.sleep(1)
+        if GPIO.input(4) == 1:
+            detect = "\nWater Detected  " + str(current_time)
+            f.write(detect)
+
+            # Command one actuator using RaspBerry Pi Zero W GPIO (4)
+            
+            led.on()
+            print(detect)
+        elif GPIO.input(4) == 0:
+            not_detected = "\nWater Not Detected " + str(current_time)
+            f.write(not_detected)
+            print(not_detected)
+            led.off()
+
+command()
